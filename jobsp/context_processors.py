e7a133bb082786c82ecda782701224136785b0a7
Ashwin <ashwin@micropyramid.com>
2021-09-06 22:24:43 +0530

    new release

--- jobsp/context_processors.py
+++ jobsp/context_processors.py
@@ -1,7 +1,7 @@
-import os
+# import os
 
 
-def export_vars(request):
-    data = {}
-    data["GOOGLE_MAPS_API_KEY"] = os.environ["GOOGLE_MAPS_API_KEY"]
-    return data
+# def export_vars(request):
+#     data = {}
+#     data["GOOGLE_MAPS_API_KEY"] = os.environ["GOOGLE_MAPS_API_KEY"]
+#     return data
